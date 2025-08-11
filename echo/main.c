ea0be5e1f1a636ddee972cec8fb427ad692179f1
LunaStev <youngjae681@gmail.com>
2025-08-11 17:19:48 +0900

    echo

--- echo/main.c
+++ echo/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main() {
-	int commands;
-
-	scanf("%d", commands);
-
-	if (commands == "echo") {
-		printf("echo commands");
+int main(int argc, char *argv[]) {
+	
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+			printf("%s", argv[i]);
+			if (i < argc - 1) {
+				printf(" ");
+			}
+		}
 	}
 
+	printf("\n");
 	return 0;
 }
